d69d9397a0ba65a67e93ffe90b829d5620374f87
Furjii <115135515+Furjii@users.noreply.github.com>
2025-01-14 00:49:48 +0700

    Setup Scene Menu
    membuat ui scene kedua, menambahkan button dan fungsinya, lalu menambahkan audio.

---
+++ Assets/Script/SceneManager1.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SceneManager1 : MonoBehaviour
+{
+    public AudioClip gameClip;
+    private AudioSource playerAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GoScene2()
+    {
+        playerAudio.PlayOneShot(gameClip, 10.0f);
+        SceneManager.LoadScene(0);
+    }
+    public void exitScene2()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}

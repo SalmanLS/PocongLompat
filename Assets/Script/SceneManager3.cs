781d20ac2cb569778d3a524401c1382c72536e01
SalmanLS <salmanluthfi456@gmail.com>
2025-01-15 04:26:24 +0700

    Singleton logic
    Save and Load name, score and wave

--- Assets/Script/SceneManager3.cs
+++ Assets/Script/SceneManager3.cs
@@ -8,10 +8,35 @@ public class SceneManager3 : MonoBehaviour
 {
     public AudioClip button;
     private AudioSource playerAudio;
+    public TextMeshProUGUI namaPlayer;
+    public TextMeshProUGUI scorePlayer;
+    public TextMeshProUGUI wavePlayer;
+
+    public TextMeshProUGUI bestName;
+    public TextMeshProUGUI bestScore;
+    public TextMeshProUGUI bestWave;
+
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
         playerAudio = GetComponent<AudioSource>();
+        namaPlayer.text = MainManager.INSTANCE.playerName;
+        scorePlayer.text = "" + MainManager.INSTANCE.playerScore;
+        wavePlayer.text = "" + MainManager.INSTANCE.playerWave;
+
+        if (MainManager.INSTANCE.playerScore > MainManager.INSTANCE.bestScore)
+        {
+            MainManager.INSTANCE.bestScore = MainManager.INSTANCE.playerScore;
+            MainManager.INSTANCE.bestWave = MainManager.INSTANCE.playerWave;
+            MainManager.INSTANCE.bestName = MainManager.INSTANCE.playerName;
+            MainManager.INSTANCE.SaveBestScore();
+        }
+        bestName.text = MainManager.INSTANCE.bestName;
+        bestScore.text = "Score : " + MainManager.INSTANCE.bestScore;
+        bestWave.text = "Wave : " + MainManager.INSTANCE.bestWave;
+
     }
 
     // Update is called once per frame

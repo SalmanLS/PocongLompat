19f361da08e2ea1cd155341077597e9b363038f0
Furjii <115135515+Furjii@users.noreply.github.com>
2025-01-15 03:49:54 +0700

    Add button function
    menambahkan perpindahan scene ketika button di klik dan menambahkan audio pada button dan button quit

--- Assets/Script/SceneManager3.cs
+++ Assets/Script/SceneManager3.cs
@@ -6,15 +6,39 @@ using UnityEngine.SceneManagement;
 
 public class SceneManager3 : MonoBehaviour
 {
+    public AudioClip button;
+    private AudioSource playerAudio;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PlayAgain()
+    {
+        playerAudio.PlayOneShot(button);
+        SceneManager.LoadScene(1);
+    }
+    public void ButtonMenu()
+    {
+        playerAudio.PlayOneShot(button);
+        StartCoroutine(TimeSwap());
+    }
+
+    public void ButtonQuit()
+    {
+        playerAudio.PlayOneShot(button);
+        Application.Quit();
+    }
+    IEnumerator TimeSwap()
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(0);
     }
 }
